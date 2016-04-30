8450fd2367497fd1f622c7c3f7c8764cb6b46258
Max Goodman <c@chromakode.com>
2016-04-29 17:15:04 -0700

    Add configurable footnoteId and footnoteCaption renderers

--- test/test.js
+++ test/test.js
@@ -12,3 +12,20 @@ describe('markdown-it-footnote', function () {
 
   generate(path.join(__dirname, 'fixtures/footnote.txt'), md);
 });
+
+describe('custom footnote ids and labels', function () {
+  var md = require('markdown-it')({
+    footnoteId: function(n, token) {
+      if (token.meta.label) {
+        return '-' + token.meta.label;
+      }
+      return n;
+    },
+
+    footnoteCaption: function(n, token) {
+      return '{' + (token.meta.label || n) + '}';
+    }
+  }).use(require('../'));
+
+  generate(path.join(__dirname, 'fixtures/custom-footnotes.txt'), md);
+});
