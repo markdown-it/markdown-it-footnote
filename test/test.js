8215b7259b0d10c1b81cb259a31acbdfdc9e2734
Vitaly Puzrin <vitaly@rcdesign.ru>
2019-03-12 05:06:36 +0300

    Remove lodash use

--- test/test.js
+++ test/test.js
@@ -1,7 +1,6 @@
 'use strict';
 
 
-var _        = require('lodash');
 var assert   = require('assert');
 var testgen  = require('markdown-it-testgen');
 var path     = require('path');
@@ -22,7 +21,7 @@ function generate(fixturePath, md, env) {
           // add variant character after "↩", so we don't have to worry about
           // invisible characters in tests
           assert.strictEqual(
-            md.render(fixture.first.text, _.clone(env || {})),
+            md.render(fixture.first.text, Object.assign({}, env || {})),
             fixture.second.text.replace(/\u21a9(?!\ufe0e)/g, '\u21a9\ufe0e')
           );
         });
